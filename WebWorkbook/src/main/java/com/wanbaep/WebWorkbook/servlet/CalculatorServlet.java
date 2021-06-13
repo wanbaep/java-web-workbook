3d3db92cd7b3d54f75968ec670301ea5cd172166
wanbaep <wanbaepp@naver.com>
2021-06-13 12:33:46 +0900

    servlet 설정 2가지 방법, web.xml, @WebServlet

---
+++ WebWorkbook/src/main/java/com/wanbaep/WebWorkbook/servlet/CalculatorServlet.java
@@ -0,0 +1,29 @@
+package com.wanbaep.WebWorkbook.servlet;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+//Servlet 3.0 사양부터는 애노테이션으로 서블릿 배치 정보를 설정할 수 있다.
+@WebServlet(urlPatterns = {"/calc","/calc.do"})
+public class CalculatorServlet extends GenericServlet {
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        int a = Integer.parseInt(servletRequest.getParameter("a"));
+        int b = Integer.parseInt(servletRequest.getParameter("b"));
+
+        servletResponse.setContentType("text/plain");
+        servletResponse.setCharacterEncoding("UTF-8");
+        PrintWriter writer = servletResponse.getWriter();
+        writer.println("a=" + a + "," + "b=" + b + " 의 계산결과 입니다.");
+        writer.println("a + b = " + (a+b));
+        writer.println("a - b = " + (a-b));
+        writer.println("a * b = " + (a*b));
+        writer.println("a / b = " + ((float)a/(float)b));
+        writer.println("a % b =" + (a % b));
+    }
+}
---
+++ WebWorkbook/src/main/java/com/wanbaep/WebWorkbook/servlet/HelloWorldGeneric.java
@@ -0,0 +1,15 @@
+package com.wanbaep.WebWorkbook.servlet;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+
+public class HelloWorldGeneric extends GenericServlet {
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println("service() 호출됨");
+        System.out.println("GenericServlet 을 상속 받으면, abstract method인 service만 구현 해서 사용 가능하다");
+    }
+}
